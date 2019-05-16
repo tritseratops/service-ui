c63ba0d42f7e42b642db9ea48c3bf6acf003a157
tritseratops <tritseratops@gmail.com>
2019-05-16 22:15:31 +0300

    EPMRPP-37238 SideBar fix, added customer isActive function to override default behavior that did not work with hash in url

--- app/src/components/buttons/sidebarButton/sidebarButton.jsx
+++ app/src/components/buttons/sidebarButton/sidebarButton.jsx
@@ -19,6 +19,10 @@
  * along with Report Portal.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import { actionToPath, getOptions, history, selectLocationState } from 'redux-first-router';
+import matchPath from 'rudy-match-path';
+import { stripBasename } from 'rudy-history/PathUtils';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import React from 'react';
 import classNames from 'classnames/bind';
@@ -28,11 +32,58 @@ import styles from './sidebarButton.scss';
 
 const cx = classNames.bind(styles);
 
-export const SidebarButton = ({ onClick, icon, children, link, bottom }) => {
+export const SidebarButtonBase = ({ onClick, icon, children, link, bottom }) => {
   const classes = cx({
     'sidebar-nav-btn': true,
     'at-bottom': bottom,
   });
+
+  /*
+ function that returns url from action and routesMap
+ used in isActive methods, can be removed once isActive will be removed
+*/
+  const toUrl = (to, routesMap) => {
+    if (typeof to === 'object') {
+      const action = to;
+
+      const { querySerializer } = getOptions();
+
+      try {
+        const path = actionToPath(action, routesMap, querySerializer);
+
+        return history().createHref({
+          pathname: path,
+        });
+      } catch (e) {
+        if (process.env.NODE_ENV === 'development') {
+          console.warn(
+            '[redux-first-router-link]couldnotcreatepathfromaction:',
+            action,
+            'Forreference,hereareyourcurrentroutes:',
+            routesMap,
+          );
+        }
+        return '#';
+      }
+    } else return '#';
+  };
+
+  /*
+   custom method for workaround for bug in redux-first-router 2.1.1 version, where paths with hash does not activate active style
+   can be removed once bug is fixed
+  */
+  const isActive = () => {
+    const to = link;
+    const options = getOptions();
+    const basename = options.basename ? options.basename : '';
+    const path = toUrl(to, location.routesMap).split('?')[0];
+
+    // deleting hash if it is present for correct matching
+    const pathWOHash = path.indexOf('#') > -1 ? `/${path.split('#')[1]}` : path;
+
+    return matchPath(location.pathname, stripBasename(pathWOHash, basename));
+  };
+
   return (
     <div className={classes}>
       <NavLink
@@ -40,6 +91,7 @@ export const SidebarButton = ({ onClick, icon, children, link, bottom }) => {
         className={cx('nav-link')}
         activeClassName={cx('active')}
         onClick={onClick}
+        isActive={isActive}
       >
         <span className={cx('wrapper')}>
           <i className={cx('btn-icon')}>{Parser(icon)}</i>
@@ -50,7 +102,7 @@ export const SidebarButton = ({ onClick, icon, children, link, bottom }) => {
   );
 };
 
-SidebarButton.propTypes = {
+SidebarButtonBase.propTypes = {
   link: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
   icon: PropTypes.string,
   bottom: PropTypes.bool,
@@ -58,10 +110,13 @@ SidebarButton.propTypes = {
   onClick: PropTypes.func,
 };
 
-SidebarButton.defaultProps = {
+SidebarButtonBase.defaultProps = {
   link: '/',
   icon: '',
   bottom: false,
   children: null,
   onClick: () => {},
 };
+
+const mapStateToProps = (state) => ({ location: selectLocationState(state) });
+export const SidebarButton = connect(mapStateToProps)(SidebarButtonBase);
